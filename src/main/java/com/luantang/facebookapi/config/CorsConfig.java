a934ca2d91ab9f90295ccb3a9bff9477e7a30c8f
LuanTang <luantang.work@gmail.com>
2024-02-19 12:18:32 +0700

    change auth

--- src/main/java/com/luantang/facebookapi/config/CorsConfig.java
+++ src/main/java/com/luantang/facebookapi/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // Define CORS configuration as needed
-        config.addAllowedOrigin("http://localhost:3000"); // Allow requests from any origin
+        config.addAllowedOrigin("*"); // Allow requests from any origin
         config.addAllowedMethod("*"); // Allow all HTTP methods
         config.addAllowedHeader("*"); // Allow all headers
         config.setAllowCredentials(true);
