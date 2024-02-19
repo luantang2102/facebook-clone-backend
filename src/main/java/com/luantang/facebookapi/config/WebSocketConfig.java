9c562cbd022d7ae44ee1f6918fab798af47ed234
LuanTang <luantang.work@gmail.com>
2024-02-19 21:59:11 +0700

    change auth

--- src/main/java/com/luantang/facebookapi/config/WebSocketConfig.java
+++ src/main/java/com/luantang/facebookapi/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/ws").withSockJS();
     }
 
     @Override
