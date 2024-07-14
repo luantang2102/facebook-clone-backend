e523bfcaa146a623103ef67fd4e629ec033c4466
LuanTang <luantang.work@gmail.com>
2024-07-15 00:54:15 +0700

    change origin

--- src/main/java/com/luantang/facebookapi/config/JWTAuthenticationFilter.java
+++ src/main/java/com/luantang/facebookapi/config/JWTAuthenticationFilter.java
@@ -21,7 +21,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class JWTAuthenticationFilter extends OncePerRequestFilter {
+public class  JWTAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private JWTService jwtService;
--- src/main/java/com/luantang/facebookapi/exceptions/UserNotFoundException.java
+++ src/main/java/com/luantang/facebookapi/exceptions/UserNotFoundException.java
@@ -1,6 +1,9 @@
 package com.luantang.facebookapi.exceptions;
 
+import java.io.Serial;
+
 public class UserNotFoundException extends RuntimeException{
+    @Serial
     private static final long serialVersionUID = 2;
     public UserNotFoundException(String message) {
         super(message);
